77046d41a8b89859fa415d27986c3d000692ebdb
Awesomefreeman <>
2020-11-30 01:06:47 +0300

    change format

--- App/create_booking.py
+++ App/create_booking.py
@@ -11,13 +11,22 @@ class Request:
 
 
 class UserInfo(dict):
-    def __init__(self, firstname, lastname, totalprice, depositpaid,
-                 bookingdates, additionalneeds):
-        dict.__init__(self, firstname=firstname, lastname=lastname, totalprice=totalprice,
-                      depositpaid=depositpaid, bookingdates=bookingdates,
+    def __init__(self, firstname,
+                 lastname,
+                 totalprice,
+                 depositpaid,
+                 bookingdates,
+                 additionalneeds):
+        dict.__init__(self, firstname=firstname,
+                      lastname=lastname,
+                      totalprice=totalprice,
+                      depositpaid=depositpaid,
+                      bookingdates=bookingdates,
                       additionalneeds=additionalneeds)
 
 
 class BookingDates(dict):
-    def __init__(self, checkin, checkout):
-        dict.__init__(self, checkin=checkin, checkout=checkout)
+    def __init__(self, checkin,
+                 checkout):
+        dict.__init__(self, checkin=checkin,
+                      checkout=checkout)
